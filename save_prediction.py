54a5352b8333a7d96e2a22005dfbc08f531b5741
GuillemBarroso <guillem.bar.gas@gmail.com>
2022-09-07 11:01:48 +0200

    clean-up + improved naming

--- save_prediction.py
+++ save_prediction.py
@@ -64,43 +64,40 @@ def getImageIndex(mus, mus_test):
         raise RuntimeError('Selected mus not found in test dataset.')
 
 if __name__ == "__main__":
+    args = Input()
+
     # Define test image to be saved
     mus = [2.2, 36]
+    args.manual_data = 11
     Fh, Fv, loc, pos = BeamHomog().getParamsFromMus(mus[0],mus[1])
 
-    print(f"f1 = {Fh}")
-    print(f"f2 = {Fv}")
-    print(f"loc = {loc}")
-    print(f"pos = {pos}")
-
     # Load model and get predicitons
-    args = Input()
     out, data = main(args)
     
     idx = getImageIndex(mus, data.mus_test)
 
-    # Compute nodal values for the triangular regular mesh from pixel values
-    nodal_vals = np.zeros([data.resolution[0]+1, data.resolution[1]+1])
-    for i in range(nodal_vals.shape[0]):
-        for j in range(nodal_vals.shape[1]):
-            if i == 0 and j==0: # bot-left corner
-                nodal_vals[i, j] = out[idx][i, j]
-            elif i == data.resolution[0] and j == 0: # bot-right corner
-                nodal_vals[i, j] = out[idx][i-1, j]
-            elif j == data.resolution[1] and i == 0: # top-left corner
-                nodal_vals[i, j] = out[idx][i, j-1]
-            elif j == data.resolution[1] and i == data.resolution[0]: # top-right corner
-                nodal_vals[i, j] = out[idx][i-1, j-1]
-            elif i == data.resolution[0]: # right boundary:
-                nodal_vals[i, j] = (out[idx][i-1,j] + out[idx][i-1, j-1])/2
-            elif i == 0: # left boundary
-                nodal_vals[i, j] = (out[idx][i,j] + out[idx][i, j-1])/2
-            elif j == data.resolution[1]: # top boundary
-                nodal_vals[i, j] = (out[idx][i,j-1] + out[idx][i-1, j-1])/2
-            elif j == 0: # bot bounary
-                nodal_vals[i, j] = (out[idx][i,j] + out[idx][i-1, j])/2
-            else: # node inisde the domain
-                nodal_vals[i, j] = (out[idx][i,j] + out[idx][i, j-1] + out[idx][i-1,j] + out[idx][i-1, j-1])/4
+    # # Compute nodal values for the triangular regular mesh from pixel values
+    # nodal_vals = np.zeros([data.resolution[0]+1, data.resolution[1]+1])
+    # for i in range(nodal_vals.shape[0]):
+    #     for j in range(nodal_vals.shape[1]):
+    #         if i == 0 and j==0: # bot-left corner
+    #             nodal_vals[i, j] = out[idx][i, j]
+    #         elif i == data.resolution[0] and j == 0: # bot-right corner
+    #             nodal_vals[i, j] = out[idx][i-1, j]
+    #         elif j == data.resolution[1] and i == 0: # top-left corner
+    #             nodal_vals[i, j] = out[idx][i, j-1]
+    #         elif j == data.resolution[1] and i == data.resolution[0]: # top-right corner
+    #             nodal_vals[i, j] = out[idx][i-1, j-1]
+    #         elif i == data.resolution[0]: # right boundary:
+    #             nodal_vals[i, j] = (out[idx][i-1,j] + out[idx][i-1, j-1])/2
+    #         elif i == 0: # left boundary
+    #             nodal_vals[i, j] = (out[idx][i,j] + out[idx][i, j-1])/2
+    #         elif j == data.resolution[1]: # top boundary
+    #             nodal_vals[i, j] = (out[idx][i,j-1] + out[idx][i-1, j-1])/2
+    #         elif j == 0: # bot bounary
+    #             nodal_vals[i, j] = (out[idx][i,j] + out[idx][i-1, j])/2
+    #         else: # node inisde the domain
+    #             nodal_vals[i, j] = (out[idx][i,j] + out[idx][i, j-1] + out[idx][i-1,j] + out[idx][i-1, j-1])/4
 
     # Get one value per trinangular element
     triang_vals = []
@@ -111,18 +108,18 @@ if __name__ == "__main__":
 
     assert len(triang_vals) == data.dimension*2, 'triang_vals shape does not match the correct number'
 
+    # Reshape
     triang_vals = np.reshape(triang_vals, [data.resolution[0]*2, data.resolution[1]], order='F')
 
     # Plot input image and NN's output (pixel, nodal values and triangular vals)
     plt.subplots(gridspec_kw={'width_ratios': [1], 'height_ratios': [1]})
-    plotImage(data.x_test[idx], 4, 1, 1)
-    plotImage(out[idx], 4, 1, 2)
-    plotImage(nodal_vals, 4, 1, 3)
-    plotImage(triang_vals, 4, 1, 4)
+    plotImage(data.x_test[idx], 3, 1, 1)
+    plotImage(out[idx], 3, 1, 2)
+    plotImage(triang_vals, 3, 1, 3)
     plt.show()
 
     # Store prediciton in txt file to be loaded in FreeFEM++
-    file_name = f"outputs_txt/mu1_{mus[0]}_mu2_{mus[1]}.txt"
+    file_name = f"outputs_txt/mu1_{mus[0]}_mu2_{mus[1]}_Fh_{Fh}_Fv_{Fv}_loc_{loc}_pos_{pos}_case_{args.manual_data}.txt"
     file = open(file_name, "w")
     file.write(f"{triang_vals.size} \n")
     file.write(f"\t")
